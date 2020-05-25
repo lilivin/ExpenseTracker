7351248684434a57d323de1ac74b0f5401a7876f
KLiwin <k.liwinski@gmail.com>
2020-05-25 08:43:56 +0200

    Updates

---
+++ precache-manifest.4ba6061688bfcfa4d0502c3bfd057bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f88f283c93a09017304a7ee4b468258",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "6b240f967a9a8488c1b2",
+    "url": "/ExpenseTracker/static/css/main.fa094317.chunk.css"
+  },
+  {
+    "revision": "0ae5556d79e340615f49",
+    "url": "/ExpenseTracker/static/js/2.f0722bc9.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/ExpenseTracker/static/js/2.f0722bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b240f967a9a8488c1b2",
+    "url": "/ExpenseTracker/static/js/main.355d19f7.chunk.js"
+  },
+  {
+    "revision": "6a4761a450062a671a7d",
+    "url": "/ExpenseTracker/static/js/runtime-main.de1c3c37.js"
+  }
+]);
